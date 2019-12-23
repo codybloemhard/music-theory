0fc6c532236a417c49f14640c687cdc979b87e50
Cody Bloemhard <cblmhrd@live.nl>
2019-12-23 17:13:05 +0100

    basics

---
+++ src/bin.rs
@@ -0,0 +1,7 @@
+extern crate music_gen;
+use music_gen::{yeet, write_test_tone};
+
+fn main(){
+    yeet();
+    write_test_tone();
+}
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use std::f32::consts::PI;
+use std::i16;
+use hound;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+pub fn yeet(){
+    println!("YEET");
+}
+
+pub fn write_test_tone(){
+    let spec = hound::WavSpec{
+        channels: 1,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("sine.wav", spec).unwrap();
+    for t in (0..44100).map(|x| x as f32 / 44100.0){
+        let sample = (t * 440.0 * 2.0 * PI).sin();
+        let ampl = i16::MAX as f32;
+        writer.write_sample((sample * ampl) as i16).unwrap();
+    }
+}
