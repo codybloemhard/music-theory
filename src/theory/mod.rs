5d28586eab8fc4dbcbbaed4cef9e6c9f6c534e59
Cody Bloemhard <cblmhrd@live.nl>
2019-12-31 18:52:11 +0100

    intervals and names

--- src/theory/mod.rs
+++ src/theory/mod.rs
@@ -2,8 +2,10 @@ pub mod note;
 pub mod scale;
 pub mod score;
 pub mod duration;
+pub mod chords;
 
 pub use note::*;
 pub use scale::*;
 pub use score::*;
 pub use duration::*;
+pub use chords::*;
--- src/theory/note.rs
+++ src/theory/note.rs
@@ -3,18 +3,122 @@ use std::convert::TryInto;
 pub type Note = i32;
 pub type Rank = u16;
 
-pub const QUAD: Note = 60;
+pub const QUAD: Note = SEMI / 2;
 pub const SEMI: Note = 120;
-pub const WHOLE: Note = 240;
+pub const WHOLE: Note = SEMI * 2;
 
-pub const MINOR_SECOND: Note = 120;
-pub const MAJOR_SECOND: Note = 240;
-pub const MINOR_THIRD: Note = 360;
-pub const MAJOR_THIRD: Note = 480;
-pub const OCTAVE: Note = 1200;
+pub const PERFECT_UNISON: Note = 0;
+pub const MINOR_SECOND: Note = SEMI;
+pub const MAJOR_SECOND: Note = WHOLE;
+pub const MINOR_THIRD: Note = SEMI * 3;
+pub const MAJOR_THIRD: Note = SEMI * 4;
+pub const PERFECT_FOURTH: Note = SEMI * 5;
+pub const TRITONE: Note = SEMI * 6;
+pub const PERFECT_FIFTH: Note = SEMI * 7;
+pub const MINOR_SIXTH: Note =  SEMI * 8;
+pub const MAJOR_SIXTH: Note = SEMI * 9;
+pub const MINOR_SEVENTH: Note = SEMI * 10;
+pub const MAJOR_SEVENTH: Note = SEMI * 11;
+pub const PERFECT_OCTAVE: Note = SEMI * 12;
 
 pub const SILENT: Note = -1;
 pub const CARRY_ON: Note = -2;
+
+pub fn interval_name(interval: Note) -> String{
+    let string = match interval{
+        0 => "Perfect Unison",
+        1 => "Minor Second",
+        2 => "Major Second",
+        3 => "Minor Third",
+        4 => "Major Third",
+        5 => "Perfect Fourth",
+        6 => "Tritone",
+        7 => "Perfect Fifth",
+        8 => "Minor Sixth",
+        9 => "Major Sixth",
+        10 => "Minor Seventh",
+        11 => "Major Seventh",
+        12 => "Perfect Octave",
+        _ => "",
+    }.to_string();
+    if string.is_empty(){
+        format!("{} Semitones", interval)
+    }else{
+        string
+    }
+}
+
+pub fn interval_name_short(interval: Note) -> String{
+    let string = match interval{
+        0 => "P1",
+        1 => "m2",
+        2 => "M2",
+        3 => "m3",
+        4 => "M3",
+        5 => "P4",
+        6 => "TT",
+        7 => "P5",
+        8 => "m6",
+        9 => "M6",
+        10 => "m7",
+        11 => "M7",
+        12 => "P8",
+        _ => "",
+    }.to_string();
+    if string.is_empty(){
+        format!("S{}", interval)
+    }else{
+        string
+    }
+}
+
+pub fn interval_name_augdim(interval: Note) -> String{
+    let string = match interval{
+        0 => "Diminished Second",
+        1 => "Augmented Unison",
+        2 => "Diminished Third",
+        3 => "Augmented Second",
+        4 => "Diminished Fourth",
+        5 => "Augmented Third",
+        6 => "Diminished Fifth/Augmented Fourth",
+        7 => "Diminished Sixth",
+        8 => "Augmented Fifth",
+        9 => "Diminished Seventh",
+        10 => "Augmented Sixth",
+        11 => "Diminished Octave",
+        12 => "Augmented Seventh",
+        _ => "",
+    }.to_string();
+    if string.is_empty(){
+        format!("{} Semitones", interval)
+    }else{
+        string
+    }
+}
+
+pub fn interval_name_augdim_short(interval: Note) -> String{
+    let string = match interval{
+        0 => "d2",
+        1 => "A1",
+        2 => "d3",
+        3 => "A2",
+        4 => "d4",
+        5 => "A3",
+        6 => "d5/A4",
+        7 => "d6",
+        8 => "A5",
+        9 => "d7",
+        10 => "A6",
+        11 => "d8",
+        12 => "A7",
+        _ => "",
+    }.to_string();
+    if string.is_empty(){
+        format!("S{}", interval)
+    }else{
+        string
+    }
+}
 
 #[derive(Clone,Copy)]
 pub enum NamedNote{
@@ -23,9 +127,9 @@ pub enum NamedNote{
 
 impl NamedNote{
     pub fn from_note(note: Note) -> Self{
-        if note % 120 == 0 { // This a a chromatic note
-            let rank: Rank = (note / 1440).max(0).try_into().unwrap();
-            let inrank = (note / 120) % 12;
+        if note % SEMI == 0 { // This a a chromatic note
+            let rank: Rank = (note / PERFECT_OCTAVE).max(0).try_into().unwrap();
+            let inrank = (note / SEMI) % 12;
             match inrank{
                 0 => NamedNote::A(rank),
                 1 => NamedNote::As(rank),
@@ -90,7 +194,7 @@ impl NamedNote{
         if x > -1 {
             return x;
         }
-        (self.rank() * 12 * 120) as Note + (self.chromatic_to_index() * 120)
+        (self.rank() as Note * 12 * SEMI) as Note + (self.chromatic_to_index() * SEMI)
     }
 
     pub fn to_string_name(self) -> String{
@@ -123,10 +227,10 @@ impl NamedNote{
             format!("{}{}", self.to_string_name(), self.rank())
         }else{
             let n = self.to_note();
-            let close_chromatic = (n / 120) * 120;
+            let close_chromatic = (n / SEMI) * SEMI;
             let diff = n - close_chromatic;
             let chroma = Self::from_note(close_chromatic);
-            format!("{}+{}",chroma.to_string(),diff as f32 / 120 as f32)
+            format!("{}+{}",chroma.to_string(),diff as f32 / SEMI as f32)
         }
     }
 }
@@ -159,8 +263,8 @@ pub fn apply_accidental_global(note: Note, acc: Accidental) -> Note{
     }
 }
 
-// note (48*120) (48=12*4) is A4 at 440 hz
+// note (48*SEMI) (48=12*4) is A4 at 440 hz
 pub fn to_pitch(note: Note) -> f32{
-    let x = note as i32 - (48*120);
-    (2.0f32).powf(x as f32 / 1440.0) * 440.0
+    let x = note as i32 - (48*SEMI);
+    (2.0f32).powf(x as f32 / PERFECT_OCTAVE as f32) * 440.0
 }
