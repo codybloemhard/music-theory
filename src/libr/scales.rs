95a621565bd5839cadc6c49382a1f5a33ad1698e
Cody Bloemhard <cblmhrd@live.nl>
2020-02-10 23:46:01 +0100

    ScaleObj and harmonic minor mode names

--- src/libr/scales.rs
+++ src/libr/scales.rs
@@ -1,9 +1,36 @@
-use crate::theory::scale::{Scale};
+use crate::theory::scale::{Scale,Mode};
 use crate::theory::interval::*;
+
+pub struct ScaleObj{
+    pub steps: Scale,
+    pub fam_name: String,
+    pub modes: Vec<String>,
+}
+
+impl ScaleObj{
+    pub fn clone_steps(&self) -> Scale{
+        self.steps.clone()
+    }
+
+    pub fn family_name(&self) -> String{
+        self.fam_name.clone()
+    }
+
+    pub fn get_mode_name(&self, mode: Mode) -> String{
+        let m = mode as usize % self.steps.len();
+        let name = self.modes[m].clone();
+        if name.is_empty(){
+            String::from("unnamed")
+        }else{
+            name
+        }
+    }
+}
 
 pub mod ionian{
     use crate::theory::scale::{Scale,Mode};
     use crate::theory::interval::*;
+    use super::ScaleObj;
 
     pub const IONIAN: Mode = 0;
     pub const DORIAN: Mode = 1;
@@ -17,17 +44,18 @@ pub mod ionian{
         vec![WHOLE,WHOLE,SEMI,WHOLE,WHOLE,WHOLE,SEMI]
     }
 
-    pub fn mode_name(mode: Mode) -> String{
-        match mode{
-            0 => "Ionian",
-            1 => "Dorian",
-            2 => "Phrygian",
-            3 => "Lydian",
-            4 => "Mixolydian",
-            5 => "Aeolian",
-            6 => "Locrian",
-            _ => "Error",
-        }.to_string()
+    pub fn obj() -> ScaleObj{
+        ScaleObj{
+            steps: steps(),
+            fam_name: String::from("Ionian"),
+            modes: vec![String::from("Ionian"),
+                        String::from("Dorian"),
+                        String::from("Phrygian"),
+                        String::from("Lydian"),
+                        String::from("Mixolidian"),
+                        String::from("Aeolian"),
+                        String::from("Locrian")]
+        }
     }
 }
 
@@ -69,12 +97,23 @@ pub mod miscellaneous_scales{
 pub mod harmonic_minor{
     use crate::theory::scale::{Scale,Mode};
     use crate::theory::interval::*;
+    use super::ScaleObj;
 
     pub fn steps() -> Scale{
         vec![WHOLE,SEMI,WHOLE,WHOLE,SEMI,MINOR_THIRD,SEMI]
     }
 
-    pub fn mode_name(mode: Mode) -> String{
-        String::from("unnamed")
+    pub fn obj() -> ScaleObj{
+        ScaleObj{
+            steps: steps(),
+            fam_name: String::from("Harmonic Minor"),
+            modes: vec![String::from("Harmonic Minor"),
+                        String::from("Locrian ♯6"),
+                        String::from("Ionian ♯5"),
+                        String::from("Dorian ♯4"),
+                        String::from("Phrygian Dominant"),
+                        String::from("Lydian ♯2"),
+                        String::from("Superlocrian")]
+        }
     }
 }
