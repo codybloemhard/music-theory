b8459c122763de36ce07a36d3e1ba5f163cf0e6a
Cody Bloemhard <cblmhrd@live.nl>
2021-01-22 22:01:24 +0100

    refactor NamedNote, simplify

--- src/theory/note.rs
+++ src/theory/note.rs
@@ -197,25 +197,32 @@ pub type PCs = Vec<PC>;
 
 impl PC{
     pub fn to_named(self, rank: Rank) -> NamedNote{
-        match self.0{
-            0  => NamedNote::A(rank),
-            1  => NamedNote::As(rank),
-            2  => NamedNote::B(rank),
-            3  => NamedNote::C(rank),
-            4  => NamedNote::Cs(rank),
-            5  => NamedNote::D(rank),
-            6  => NamedNote::Ds(rank),
-            7  => NamedNote::E(rank),
-            8  => NamedNote::F(rank),
-            9  => NamedNote::Fs(rank),
-            10 => NamedNote::G(rank),
-            11 => NamedNote::Gs(rank),
-            _ => panic!("PC::to_named: should never happen!"),
+        NamedNote{
+            pc: self,
+            rank,
         }
     }
 
     pub fn to_note(self, rank: Rank) -> Note{
-        self.0 + (rank as Note * PERFECT_OCTAVE)
+        (self.0 * SEMI) + (rank as Note * PERFECT_OCTAVE)
+    }
+
+    pub fn to_string_name(self) -> String{
+        match self.0{
+            0  => "A",
+            1  => "A♯",
+            2  => "B",
+            3  => "C",
+            4  => "C♯",
+            5  => "D",
+            6  => "D♯",
+            7  => "E",
+            8  => "F",
+            9  => "F♯",
+            10  => "G",
+            11 => "G♯",
+            _ => panic!("PC::to_string_name: impossible"),
+        }.to_string()
     }
 }
 
@@ -237,7 +244,7 @@ pub fn as_pc(note: Note) -> PC{
         if inrank >= 12 { panic!("as_pc: should never happen!"); }
         PC(inrank)
     } else { // This is a microtonal note
-        panic!("to_ucn: microtonal input");
+        panic!("to_pc: microtonal input");
     }
 }
 
@@ -349,115 +356,37 @@ impl IntoSteps for PCs{
 }
 
 #[derive(Clone,Copy)]
-pub enum NamedNote{
-    A(Rank), As(Rank), B(Rank), C(Rank), Cs(Rank), D(Rank), Ds(Rank), E(Rank), F(Rank), Fs(Rank), G(Rank), Gs(Rank), MicroTonal(Note)
+pub struct NamedNote{
+    pc: PC,
+    rank: Rank,
 }
 
 impl NamedNote{
     pub fn from_note(note: Note) -> Self{
-        if note % SEMI == 0 { // This a a chromatic note
-            let rank: Rank = (note / PERFECT_OCTAVE).max(0).try_into().unwrap();
-            let mut inrank = (note / SEMI) % 12;
-            if inrank < 0 { inrank += 12; }
-            match inrank{
-                0 => NamedNote::A(rank),
-                1 => NamedNote::As(rank),
-                2 => NamedNote::B(rank),
-                3 => NamedNote::C(rank),
-                4 => NamedNote::Cs(rank),
-                5 => NamedNote::D(rank),
-                6 => NamedNote::Ds(rank),
-                7 => NamedNote::E(rank),
-                8 => NamedNote::F(rank),
-                9 => NamedNote::Fs(rank),
-                10 => NamedNote::G(rank),
-                11 => NamedNote::Gs(rank),
-                _ => { panic!("NamedNote::from_note: should never happen!"); }
-            }
-        } else { // This is a microtonal note
-            NamedNote::MicroTonal(note)
-        }
+        let rank: Rank = (note / PERFECT_OCTAVE).max(0).try_into().unwrap();
+        let mut inrank = (note / SEMI) % 12;
+        if inrank < 0 { inrank += 12; }
+        Self{ pc: PC(inrank), rank }
     }
 
-    pub fn rank(self) -> Rank{
-        match self{
-            NamedNote::A(r)     => r,
-            NamedNote::As(r)    => r,
-            NamedNote::B(r)     => r,
-            NamedNote::C(r)     => r,
-            NamedNote::Cs(r)    => r,
-            NamedNote::D(r)     => r,
-            NamedNote::Ds(r)    => r,
-            NamedNote::E(r)     => r,
-            NamedNote::F(r)     => r,
-            NamedNote::Fs(r)    => r,
-            NamedNote::G(r)     => r,
-            NamedNote::Gs(r)    => r,
-            NamedNote::MicroTonal(n) => (n / 1440) as Rank,
-        }
+    pub fn rank(&self) -> Rank{
+        self.rank
     }
 
-    pub fn chromatic_to_index(self) -> Note{
-        match self{
-            NamedNote::A(_)     => 0,
-            NamedNote::As(_)    => 1,
-            NamedNote::B(_)     => 2,
-            NamedNote::C(_)     => 3,
-            NamedNote::Cs(_)    => 4,
-            NamedNote::D(_)     => 5,
-            NamedNote::Ds(_)    => 6,
-            NamedNote::E(_)     => 7,
-            NamedNote::F(_)     => 8,
-            NamedNote::Fs(_)    => 9,
-            NamedNote::G(_)     => 10,
-            NamedNote::Gs(_)    => 11,
-            _ => 0,
-        }
+    pub fn pc(&self) -> PC{
+        self.pc
     }
 
     pub fn to_note(self) -> Note{
-        let x = match self{
-            NamedNote::MicroTonal(n) => n,
-            _ => -1,
-        };
-        if x > -1 {
-            return x;
-        }
-        (self.rank() as Note * 12 * SEMI) as Note + (self.chromatic_to_index() * SEMI)
+        self.pc.to_note(self.rank)
     }
 
-    pub fn to_string_name(self) -> String{
-        match self{
-            NamedNote::A(_)     => "A",
-            NamedNote::As(_)    => "A♯",
-            NamedNote::B(_)     => "B",
-            NamedNote::C(_)     => "C",
-            NamedNote::Cs(_)    => "C♯",
-            NamedNote::D(_)     => "D",
-            NamedNote::Ds(_)    => "D♯",
-            NamedNote::E(_)     => "E",
-            NamedNote::F(_)     => "F",
-            NamedNote::Fs(_)    => "F♯",
-            NamedNote::G(_)     => "G",
-            NamedNote::Gs(_)    => "G♯",
-            NamedNote::MicroTonal(_) => "X",
-        }.to_string()
-    }
-
-    pub fn is_chromatic(self) -> bool{
-        !matches!(self, Self::MicroTonal(_))
+    pub fn to_string_name(&self) -> String{
+        self.pc.to_string_name()
     }
 
     pub fn as_string(self) -> String{
-        if self.is_chromatic() {
-            format!("{}{}", self.to_string_name(), self.rank())
-        }else{
-            let n = self.to_note();
-            let close_chromatic = (n / SEMI) * SEMI;
-            let diff = n - close_chromatic;
-            let chroma = Self::from_note(close_chromatic);
-            format!("{}+{}",chroma.to_string(),diff as f32 / SEMI as f32)
-        }
+        format!("{}{}", self.to_string_name(), self.rank())
     }
 }
 
@@ -474,34 +403,11 @@ impl std::fmt::Display for NamedNote{
 36  37  38  39  40  41  42  43  44  45  46  47  // rank 3
 48                                              // A4
 */
-#[derive(Clone,Copy)]
-pub enum Accidental{
-    Sharp, Flat, Natural, DoubleSharp, DoubleFlat,
-}
-
-pub fn apply_accidental_global(note: Note, acc: Accidental) -> Note{
-    match acc{
-        Accidental::Sharp => note + SEMI,
-        Accidental::Flat => note - SEMI,
-        Accidental::DoubleSharp => note + WHOLE,
-        Accidental::DoubleFlat => note - WHOLE,
-        Accidental::Natural => note // can't apply natural without context
-    }
-}
 
 // note (48*SEMI) (48=12*4) is A4 at 440 hz
 pub fn to_pitch(note: Note) -> f32{
     let x = note as i32 - (48*SEMI);
     (2.0f32).powf(x as f32 / PERFECT_OCTAVE as f32) * 440.0
-}
-
-pub fn print_notes(scale: &[Note], seperator: &str){
-    if scale.is_empty() { return; }
-    let lenm1 = scale.len() - 1;
-    for note in scale.iter().take(lenm1){
-        print!("{}{}", NamedNote::from_note(*note).as_string(), seperator);
-    }
-    println!("{}", NamedNote::from_note(scale[lenm1]).as_string());
 }
 
 #[cfg(test)]
